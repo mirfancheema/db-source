70022291debfb84d1179793bc63c37e1c5db8192
Irfan <mirfancheema@gmail.com>
2021-08-09 01:55:48 -0700

    added file playbook

---
+++ files/check-windows-login-exists.ps1
@@ -0,0 +1,78 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [Alias('hostname')]
+    [string]$computer,
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [string]$sqlinstance,
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [string[]]$LoginName,
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [string]$Domain
+  )
+$ErrorActionPreference ='Stop'
+#  Write-Host $computer
+#  Write-Host $sqlinstance
+  # Write-Host $LoginName
+#  for ($i = 0; $i -lt $LoginName.length; $i++){
+#     Write-Host $LoginName[$i]
+#     }
+#
+Function CHECK_LOGIN_EXISTS{
+                [CmdletBinding()]
+                Param(
+                                [Parameter(Mandatory=$true)]
+                                [Alias('instance')]
+                                [string]$sqlinstance,
+                                [Parameter(Mandatory=$true)]
+                                [Alias('login')]
+                                [string]$LoginName,
+                                [Parameter(Mandatory=$true)]
+                                [Alias('domainname')]
+                                [string]$Domain
+                )
+               BEGIN{
+                               Try{
+ $query1 = @"
+               SELECT name FROM master.sys.server_principals WHERE name = '$Domain\$LoginName'
+"@                                              
+                                               $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+                                               $SqlConnection.ConnectionString = "Data Source = $sqlinstance; Initial Catalog = Master; Integrated Security=true; Connection Timeout=60;"
+                                               $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+                                               $SqlCmd.CommandText = $query1
+                                               $SqlCmd.Connection = $SqlConnection
+                                               $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+                                               $SqlAdapter.SelectCommand = $SqlCmd
+                                               $DataSet = New-Object System.Data.DataSet
+                                                $SqlAdapter.Fill($DataSet) | Out-Null
+                                                $ErrorActionPreference = "Continue"
+                                }
+                                Catch{
+                                               Write-Host "Login Creation Failed" -ForegroundColor "Red"
+                                }
+                }
+                Process{
+#                                Write-Host "$query1"
+                                $DataSet.Tables[0]
+#                                return $DataSet.Tables[0]
+                                return $true
+                }
+                END{
+                                $SqlConnection.Close()
+                                 Write-Host "END"
+                }
+
+}
+
+
+  for ($i = 0; $i -lt $LoginName.length; $i++){
+     Write-Host $LoginName[$i]
+  $lgin = $LoginName[$i]
+  $LoginCheck = CHECK_LOGIN_EXISTS -instance $sqlinstance -login $LoginName[$i] -domainname $Domain
+  if ( $LoginCheck )
+  {Write-Error "Login $lgin Exists" -ErrorAction Continue
+   exit 1
+     }
+  }
+
+  exit 0
